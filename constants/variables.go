8511188898c3a8c30068ea3b84e51bec0628ee38
SwarzChen <aaaa102234@gmail.com>
2022-03-25 20:42:40 +0800

    🍡 [main] add some new env

--- constants/variables.go
+++ constants/variables.go
@@ -8,6 +8,10 @@ var SQL_USER string
 var SQL_PASSWORD string
 var DATABASE_NAME string
 var DATABASE_SOCKET_DIR string
+var POSTGRESQL_IP string
+var REDIS_IP string
+var POSTGRESQL_PORT_NUMBER string
+var REDIS_PORT_NUMBER string
 
 func FetchEnvVariables() error {
 	var isExisted bool
@@ -29,6 +33,26 @@ func FetchEnvVariables() error {
 	DATABASE_SOCKET_DIR, isExisted = os.LookupEnv("DB_SOCKET_DIR")
 	if !isExisted {
 		DATABASE_SOCKET_DIR = "/cloudsql"
+	}
+
+	POSTGRESQL_IP, isExisted = os.LookupEnv("POSTGRESQL_IP")
+	if !isExisted {
+		return SOME_ENV_VARIABLE_MISSING_ERROR
+	}
+
+	REDIS_IP, isExisted = os.LookupEnv("REDIS_IP")
+	if !isExisted {
+		return SOME_ENV_VARIABLE_MISSING_ERROR
+	}
+
+	POSTGRESQL_PORT_NUMBER, isExisted = os.LookupEnv("POSTGRESQL_PORT_NUMBER")
+	if !isExisted {
+		return SOME_ENV_VARIABLE_MISSING_ERROR
+	}
+
+	REDIS_PORT_NUMBER, isExisted = os.LookupEnv("REDIS_PORT_NUMBER")
+	if !isExisted {
+		return SOME_ENV_VARIABLE_MISSING_ERROR
 	}
 
 	return nil
