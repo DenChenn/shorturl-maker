108930633267842bb5883ab85b8a50d6611d6ca5
SwarzChen <aaaa102234@gmail.com>
2022-03-25 20:40:35 +0800

    🍤 [main] remove create table function

--- server.go
+++ server.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"github.com/SwarzChen/url-shortener/router"
-	"github.com/SwarzChen/url-shortener/utils"
-	"log"
 )
 
 func main() {
-	// create required table
-	err := utils.CreateTable()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	engine := router.InitRoutes()
 	panic(engine.Run())
 }
