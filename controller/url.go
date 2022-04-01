657304ee4b4a5c3631b894f32c15e652f0852d33
SwarzChen <aaaa102234@gmail.com>
2022-04-01 16:18:45 +0800

    ⚡️ [main] improve performance with gin context and add new short id generation technique

--- controller/url.go
+++ controller/url.go
@@ -1,13 +1,13 @@
 package controller
 
 import (
-	"context"
+	"database/sql"
 	"encoding/json"
 	"github.com/SwarzChen/url-shortener/constants"
 	"github.com/SwarzChen/url-shortener/database"
 	"github.com/SwarzChen/url-shortener/model"
 	"github.com/gin-gonic/gin"
-	"github.com/segmentio/ksuid"
+	"github.com/teris-io/shortid"
 	"io/ioutil"
 	"net/http"
 	"time"
@@ -18,12 +18,16 @@ func GetUrlHandler(c *gin.Context) {
 
 	postgreSQLClient := database.DatabaseClient.PostgreSQLClient
 	redisClient := database.DatabaseClient.RedisClient
-	ctx := context.Background()
+	ctx := c.Request.Context()
 
+	var urlEntity model.UrlEntityInDB
 	// get url
-	rows, err := postgreSQLClient.Query("SELECT * FROM url WHERE id=$1", urlId)
-	// id query cause error
-	if err != nil {
+	err := postgreSQLClient.QueryRowContext(ctx, "SELECT * FROM url WHERE id=$1", urlId).Scan(&urlEntity.Id,
+		&urlEntity.Url, &urlEntity.ExpireAt, &urlEntity.CreateAt)
+
+	switch {
+	// not exist in database
+	case err == sql.ErrNoRows:
 		// record to redis
 		err = redisClient.Set(ctx, urlId, constants.ID_NOT_EXIST, time.Hour).Err()
 		if err != nil {
@@ -35,52 +39,22 @@ func GetUrlHandler(c *gin.Context) {
 
 		c.JSON(constants.URL_NOT_EXIST_OR_EXPIRE.StatusCode, constants.URL_NOT_EXIST_OR_EXPIRE)
 		return
-	}
 
-	var urlEntity model.UrlEntityInDB
-	count := 0
-	for rows.Next() {
-		count++
-		err := rows.Scan(&urlEntity.Id, &urlEntity.Url, &urlEntity.ExpireAt, &urlEntity.CreateAt)
-		if err != nil {
-			errorCopy := constants.POSTGRESQL_ROW_SCANNING_ERROR
-			errorCopy.Message = err.Error()
-			c.JSON(errorCopy.StatusCode, errorCopy)
-			return
-		}
-	}
-	// Not found any row
-	if count == 0 {
-		// record to redis
-		err = redisClient.Set(ctx, urlId, constants.ID_NOT_EXIST, time.Hour).Err()
-		if err != nil {
-			errorCopy := constants.REDIS_INSERTION_ERROR
-			errorCopy.Message = err.Error()
-			c.JSON(errorCopy.StatusCode, errorCopy)
-			return
-		}
-
-		c.JSON(constants.URL_NOT_EXIST_OR_EXPIRE.StatusCode, constants.URL_NOT_EXIST_OR_EXPIRE)
+	// other kinds database error
+	case err != nil:
+		errorCopy := constants.POSTGRESQL_QUERY_ERROR
+		errorCopy.Message = err.Error()
+		c.JSON(errorCopy.StatusCode, errorCopy)
 		return
 	}
 
 	expireUTCTime, _ := time.Parse(time.RFC3339, urlEntity.ExpireAt)
-
-	// specify time zone
-	loc, _ := time.LoadLocation(constants.TIME_ZONE)
-	currentTime := time.Now().In(loc)
+	currentTime := time.Now().In(constants.TIME_ZONE)
 
 	// This url expired, we will delete it
 	if currentTime.After(expireUTCTime) {
-		stmt, err := postgreSQLClient.Prepare("DELETE FROM url WHERE id=$1")
-		if err != nil {
-			errorCopy := constants.SCHEMA_PREPARATION_FAIL_ERROR
-			errorCopy.Message = err.Error()
-			c.JSON(errorCopy.StatusCode, errorCopy)
-			return
-		}
-
-		_, err = stmt.Exec(urlId)
+		// remove expired url from database
+		_, err = postgreSQLClient.ExecContext(ctx, "DELETE FROM url WHERE id=$1", urlId)
 		if err != nil {
 			errorCopy := constants.POSTGRESQL_DELETION_ERROR
 			errorCopy.Message = err.Error()
@@ -147,10 +121,8 @@ func CreateUrlHandler(c *gin.Context) {
 		return
 	}
 
-	// specify time zone
-	loc, _ := time.LoadLocation(constants.TIME_ZONE)
-	currentTime := time.Now().In(loc)
-	currentTimeString := time.Now().In(loc).Format(time.RFC3339)
+	currentTime := time.Now().In(constants.TIME_ZONE)
+	currentTimeString := time.Now().In(constants.TIME_ZONE).Format(time.RFC3339)
 
 	// check expire time validation
 	if expireUTCTime.Before(currentTime) {
@@ -158,20 +130,29 @@ func CreateUrlHandler(c *gin.Context) {
 		return
 	}
 
-	// generate primitive id
-	uuidString := ksuid.New().String()
+	// generate short unique id
+	generator, err := shortid.New(1, shortid.DefaultABC, 2342)
+	if err != nil {
+		errorCopy := constants.SETTING_GENERATOR_SEED_ERROR
+		errorCopy.Message = err.Error()
+		c.JSON(errorCopy.StatusCode, errorCopy)
+		return
+	}
+
+	sid, err := generator.Generate()
+	if err != nil {
+		errorCopy := constants.ID_GENERATION_ERROR
+		errorCopy.Message = err.Error()
+		c.JSON(errorCopy.StatusCode, errorCopy)
+		return
+	}
 
 	// insert into postgresql
+	ctx := c.Request.Context()
 	postgreSQLClient := database.DatabaseClient.PostgreSQLClient
-	stmt, err := postgreSQLClient.Prepare("INSERT INTO url (id, url, expire_at, create_at) VALUES ($1, $2, $3, $4);")
-	if err != nil {
-		errorCopy := constants.SCHEMA_PREPARATION_FAIL_ERROR
-		errorCopy.Message = err.Error()
-		c.JSON(errorCopy.StatusCode, errorCopy)
-		return
-	}
 
-	_, err = stmt.Exec(uuidString, urlEntity.Url, urlEntity.ExpireAt, currentTimeString)
+	_, err = postgreSQLClient.ExecContext(ctx, "INSERT INTO url (id, url, expire_at, create_at) VALUES ($1, $2, $3, "+
+		"$4);", sid, urlEntity.Url, urlEntity.ExpireAt, currentTimeString)
 	if err != nil {
 		errorCopy := constants.POSTGRESQL_INSERTION_ERROR
 		errorCopy.Message = err.Error()
@@ -179,28 +160,10 @@ func CreateUrlHandler(c *gin.Context) {
 		return
 	}
 
-	// take back database data to check and response
-	rows, err := postgreSQLClient.Query("SELECT * FROM url WHERE id=$1", uuidString)
-	if err != nil {
-		errorCopy := constants.POSTGRESQL_INSERTION_ERROR
-		errorCopy.Message = err.Error()
-		c.JSON(errorCopy.StatusCode, errorCopy)
-		return
+	urlResponseEntity := model.UrlEntity{
+		Id:       sid,
+		ShortUrl: constants.SERVER_HOST + "/" + constants.CURRENT_VERSION + "/" + sid,
 	}
-
-	var urlResponseEntity model.UrlEntity
-	var ignore string
-	for rows.Next() {
-		err := rows.Scan(&urlResponseEntity.Id, &ignore, &ignore, &ignore)
-		if err != nil {
-			errorCopy := constants.POSTGRESQL_ROW_SCANNING_ERROR
-			errorCopy.Message = err.Error()
-			c.JSON(errorCopy.StatusCode, errorCopy)
-			return
-		}
-	}
-
-	urlResponseEntity.ShortUrl = constants.SERVER_HOST + "/" + constants.CURRENT_VERSION + "/" + uuidString
 
 	c.JSON(http.StatusOK, urlResponseEntity)
 	return
